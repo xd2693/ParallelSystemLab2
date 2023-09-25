879298bdb1207be05505ed4de37706ab355025b6
Xufei Dai <dai.xufei@gmail.com>
2023-09-25 10:30:48 -0700

    shmem test

--- src/kmeans_kernel.cuh
+++ src/kmeans_kernel.cuh
@@ -43,3 +43,22 @@ __global__ void get_label_shared(double *input_vals_c,
                          double *temp_centers_c,
                          int *n_points_c,
                          int work_per_thread);
+
+void wrapper_get_label_shmem(double *input_vals_c, 
+                         double *centers_c,
+                         int    *labels_c,
+                         int    dims,
+                         int    n_vals,
+                         int    n_cluster,
+                         double *temp_centers_c,
+                         int *n_points_c,
+                         int threads);
+                         
+__global__ void get_label_shmem(double *input_vals_c, 
+                         double *centers_c, 
+                         int    *labels_c,
+                         int    dims,
+                         int    n_vals,
+                         int    n_cluster,
+                         double *temp_centers_c,
+                         int *n_points_c);
