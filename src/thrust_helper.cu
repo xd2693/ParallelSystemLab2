095c4a7f853fad7e1b087e85e797d9df46e8060a
Xufei Dai <dai.xufei@gmail.com>
2023-09-25 20:22:26 -0700

    start code for thrust

---
+++ src/thrust_helper.cu
@@ -0,0 +1,40 @@
+#include "thrust_helper.cuh"
+#include <iostream>
+
+/*void get_label_thrust(thrust::device_vector<double> & input_vals, 
+                      thrust::device_vector<double> & old_centers,
+                      thrust::device_vector<int> & labels,
+                      thrust::device_vector<int> & labels_for_reduce,
+                      thrust::device_vector<double> & new_centers, 
+                      thrust::device_vector<int> & n_points,
+                      int    dims,
+                      int    n_vals,
+                      int    n_cluster)
+{
+    thrust::counting_iterator<int> first_point(0);
+    thrust::counting_iterator<int> last_point = first_point + n_vals;
+
+}*/
+
+void get_label_thrust(thrust::device_vector<double> & input_vals, 
+                      thrust::device_vector<double> & old_centers,
+                      thrust::device_vector<double> & new_centers,
+                      thrust::device_vector<int> & labels,
+                      thrust::device_vector<int> & labels_for_reduce,
+                      thrust::device_vector<int> & n_points,
+                      int    dims,
+                      int    n_vals,
+                      int    n_cluster)
+{
+    thrust::counting_iterator<int> first_point(0);
+    thrust::counting_iterator<int> last_point = first_point + n_vals-1;
+    double* input_vals_p = thrust::raw_pointer_cast(input_vals.data());
+    double* old_centers_p = thrust::raw_pointer_cast(old_centers.data());
+    //double* new_centers_p = thrust::raw_pointer_cast(new_centers.data());
+    int* labels_p = thrust::raw_pointer_cast(labels.data());
+    int* labels_reduce_p = thrust::raw_pointer_cast(labels_for_reduce.data());
+    int* n_points_p = thrust::raw_pointer_cast(n_points.data());
+
+    CentoidAssignFunctor functor(input_vals_p, old_centers_p, labels_p, labels_reduce_p, n_points_p, dims, n_cluster);
+    thrust::for_each(first_point, last_point, functor);
+}
---
+++ src/thrust_helper.cuh
@@ -0,0 +1,41 @@
+#pragma once
+#include <thrust/device_vector.h>
+#include <thrust/transform.h>
+#include <thrust/sequence.h>
+#include <thrust/copy.h>
+#include <thrust/fill.h>
+#include <thrust/replace.h>
+#include <thrust/functional.h>
+#include <iostream>
+
+struct CentoidAssignFunctor {
+    double * input_vals;
+    double * center_vals;
+    int * label_vals;
+    int * label_vals_reduce;
+    int * own_points;
+    int dims;
+    int clusters;
+
+    CentoidAssignFunctor(double* input, double* center, int* label, int* label_reduce, int* points, int dims, int clusters)
+    : input_vals(input), center_vals(center), label_vals(label), label_vals_reduce(label_reduce), own_points(points), dims(dims), clusters(clusters)
+    {}
+
+    __host__ __device__
+    void operator()(int& index) const {
+        for (int i = 0; i < dims; i++) {
+            intput_vals[index*dims+i] += i;
+        }
+        label_vals[index] = index;
+    }
+}
+
+void get_label_thrust(thrust::device_vector<double> & input_vals, 
+                      thrust::device_vector<double> & old_centers,
+                      thrust::device_vector<double> & new_centers,
+                      thrust::device_vector<int> & labels,
+                      thrust::device_vector<int> & labels_for_reduce,
+                      thrust::device_vector<int> & n_points,
+                      int    dims,
+                      int    n_vals,
+                      int    n_cluster);
